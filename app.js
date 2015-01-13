3c81c510b1ee89837da8fca79ca7be66a34dc7d4
pepelazz <pepelazz00@gmail.com>
2015-01-13 15:36:59 +0400

    1

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"./src/javascript/app.coffee":[function(require,module,exports){
-require('./ng-app');
-
 require('./util');
 
 require('./loader');
 
+require('./ng-app');
+
+require('./projects');
 
 
-},{"./loader":"/Users/Trikster/static_sites/Uniservice/_Uniservice/src/javascript/loader.coffee","./ng-app":"/Users/Trikster/static_sites/Uniservice/_Uniservice/src/javascript/ng-app.coffee","./util":"/Users/Trikster/static_sites/Uniservice/_Uniservice/src/javascript/util.coffee"}],"/Users/Trikster/static_sites/Uniservice/_Uniservice/src/javascript/loader.coffee":[function(require,module,exports){
+
+},{"./loader":"/Users/Trikster/static_sites/Uniservice/_Uniservice/src/javascript/loader.coffee","./ng-app":"/Users/Trikster/static_sites/Uniservice/_Uniservice/src/javascript/ng-app.coffee","./projects":"/Users/Trikster/static_sites/Uniservice/_Uniservice/src/javascript/projects.coffee","./util":"/Users/Trikster/static_sites/Uniservice/_Uniservice/src/javascript/util.coffee"}],"/Users/Trikster/static_sites/Uniservice/_Uniservice/src/javascript/loader.coffee":[function(require,module,exports){
 $(function() {
   $('#hideAll .loader').css({
     marginTop: window.innerHeight * 0.4
@@ -24,22 +26,11 @@ $(function() {
 },{}],"/Users/Trikster/static_sites/Uniservice/_Uniservice/src/javascript/ng-app.coffee":[function(require,module,exports){
 var module;
 
-module = angular.module('app', ['ui.router']);
+module = angular.module('app', ['ui.router', 'projects']);
 
 module.config([
   '$urlRouterProvider', '$stateProvider', '$locationProvider', '$sceProvider', (function($urlRouterProvider, $stateProvider, $locationProvider, $sceProvider) {
     $urlRouterProvider.otherwise('/');
-    $stateProvider.state({
-      name: "home",
-      url: "/",
-      views: {
-        '': {
-          templateUrl: function() {
-            return "templates/first-page.html";
-          }
-        }
-      }
-    });
     $stateProvider.state({
       name: "contacts",
       url: "/contacts",
@@ -47,17 +38,23 @@ module.config([
         '': {
           templateUrl: function() {
             return "templates/contacts.html";
+          },
+          controller: function($scope, $rootScope) {
+            return $rootScope.firstScreenIsShown = false;
           }
         }
       }
     });
     $stateProvider.state({
-      name: "projects",
-      url: "/projects",
+      name: "about",
+      url: "/about",
       views: {
         '': {
           templateUrl: function() {
-            return "templates/projects.html";
+            return "templates/about.html";
+          },
+          controller: function($scope, $rootScope) {
+            return $rootScope.firstScreenIsShown = false;
           }
         }
       }
@@ -65,7 +62,45 @@ module.config([
   })
 ]);
 
-module.controller('main', ['$rootScope', '$scope', '$http', '$location', '$timeout', '$log', (function($rootScope, $scope, $http, $location, $timeout, $log) {})]);
+module.controller('main', [
+  '$rootScope', '$scope', '$http', '$location', '$timeout', '$log', (function($rootScope, $scope, $http, $location, $timeout, $log) {
+    $rootScope.firstScreenIsShown = true;
+    $scope.firstScreen = {
+      show: function() {
+        return $rootScope.firstScreenIsShown = true;
+      },
+      hide: function() {
+        return $rootScope.firstScreenIsShown = false;
+      }
+    };
+  })
+]);
+
+
+
+},{}],"/Users/Trikster/static_sites/Uniservice/_Uniservice/src/javascript/projects.coffee":[function(require,module,exports){
+var module;
+
+module = angular.module('projects', ['ui.router']);
+
+module.config([
+  '$urlRouterProvider', '$stateProvider', (function($urlRouterProvider, $stateProvider) {
+    $stateProvider.state({
+      name: "projects",
+      url: "/projects",
+      views: {
+        '': {
+          templateUrl: function() {
+            return "templates/projects.html";
+          },
+          controller: function($scope, $rootScope) {
+            return $rootScope.firstScreenIsShown = false;
+          }
+        }
+      }
+    });
+  })
+]);
 
 
 
